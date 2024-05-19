1d40a1bf2ef373afd36d861708806784d3aeed30
sigmundk <sigmund.klapbakken@orbitntnu.com>
2024-05-19 15:33:23 +0200

    drivers: timer: tms570: Use clock_control API

--- drivers/timer/tms570_timer.c
+++ drivers/timer/tms570_timer.c
@@ -1,12 +1,17 @@
 
+#include <zephyr/devicetree.h>
 #include <zephyr/arch/cpu.h>
 #include <zephyr/drivers/timer/system_timer.h>
 #include <zephyr/irq.h>
+#include <zephyr/drivers/clock_control.h>
 
 #define DT_DRV_COMPAT  tms570_timer
 #define DRV_REG        DT_INST_REG_ADDR(0)
 #define DRV_INTERRUPT  DT_INST_IRQN(0)
 #define DRV_CLOCK_FREQ DT_INST_PROP(0, clock_frequency)
+
+static const unsigned int drv_clock_id = DT_INST_CLOCKS_CELL(0, clk_id);
+static const struct device *const clk_ctrl = DEVICE_DT_GET(DT_NODELABEL(clock));
 
 #define GCTRL_OFFSET      (0x00) /* Global control register */
 #define CPU0_OFFSET       (0x18) /* Compare up counter 0 */
@@ -16,7 +21,6 @@
 #define INTFLAG_OFFSET    (0x88) /* Interrupt flag */
 
 #define CPU0    (1)
-#define UDC0    (DRV_CLOCK_FREQ / (CONFIG_SYS_CLOCK_TICKS_PER_SEC / (CPU0 + 1)))
 #define CNT0EN  (1 << 0) /* Counter 0 enable */
 #define INT0EN  (1 << 0) /* Interrupt 0 enable */
 #define INT0CLR (1 << 0) /* Clear interrupt 0 */
@@ -35,6 +39,22 @@ void sys_clock_set_timeout(int32_t ticks, bool idle)
         ARG_UNUSED(idle);
 }
 
+static int set_udc0(void)
+{
+        uint32_t rate;
+        int ret;
+
+        ret = clock_control_get_rate(clk_ctrl, (clock_control_subsys_t)&drv_clock_id, &rate);
+        if (ret < 0) {
+                return ret;
+        }
+
+        rate /= CONFIG_SYS_CLOCK_TICKS_PER_SEC / (CPU0 + 1);
+        sys_write32(rate, DRV_REG + UDC0_OFFSET);
+
+        return 0;
+}
+
 static void timer_isr(void *arg)
 {
         ARG_UNUSED(arg);
@@ -47,13 +67,21 @@ static void timer_isr(void *arg)
 
 static int timer_init(void)
 {
+        int ret = clock_control_on(clk_ctrl, (clock_control_subsys_t)&drv_clock_id);
+        if (ret < 0) {
+                return ret;
+        }
+
         /* Temporaribly disable counter */
         sys_write32(0, DRV_REG + GCTRL_OFFSET);
 
         /* Set periodicity of interrupts. This is derived from the equation:
          * t_compx = t_rticlk * (RTICPUCy + 1) * (RTIUDCPy) */
         sys_write32(CPU0, DRV_REG + CPU0_OFFSET);
-        sys_write32(UDC0, DRV_REG + UDC0_OFFSET);
+        ret = set_udc0();
+        if (ret < 0) {
+                return ret;
+        }
 
         IRQ_CONNECT(DRV_INTERRUPT, 0, timer_isr, NULL, 0);
         irq_enable(DRV_INTERRUPT);
@@ -67,4 +95,4 @@ static int timer_init(void)
         return 0;
 }
 
-SYS_INIT(timer_init, PRE_KERNEL_2, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
+SYS_INIT(timer_init, PRE_KERNEL_1, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
