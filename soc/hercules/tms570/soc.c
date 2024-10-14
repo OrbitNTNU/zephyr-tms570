ab9975f0ab5c4d9fdc214cb58fdb91d91faa1f5c
sigmundk <sigmund.klapbakken@orbitntnu.com>
2024-10-14 18:11:34 +0200

    soc: tms570: Implement sys_arch_reboot

--- soc/hercules/tms570/soc.c
+++ soc/hercules/tms570/soc.c
@@ -6,6 +6,10 @@
 #define PSPWRDWNCLR_OFFSET (0xa0)
 #define PSPWRDWNCLR_COUNT  (4)
 
+#define SYS_REG    (0xffffff00)
+#define ECR_OFFSET (0xe0)
+#define RESET_BIT  (1 << 15)
+
 extern void _errata_CORTEXR4_66_(void);
 extern void _errata_CORTEXR4_57_(void);
 
@@ -15,6 +19,13 @@ static int pcr_init(void)
                 sys_write32((1ULL << 32) - 1, PCR_REG + PSPWRDWNCLR_OFFSET + i * sizeof(uint32_t));
         }
         return 0;
+}
+
+void sys_arch_reboot(int type)
+{
+        ARG_UNUSED(type);
+
+        sys_set_bits(SYS_REG + ECR_OFFSET, RESET_BIT);
 }
 
 void z_arm_platform_init(void)
