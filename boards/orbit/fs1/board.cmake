8f39f9d0f98ed6f69b159122aad41554a2e1ed5c
sigmundk <sigmund.klapbakken@orbitntnu.com>
2024-10-15 18:31:10 +0200

    boards: fs1: Remove OpenOCD support
    Not being used

--- boards/orbit/fs1/board.cmake
+++ boards/orbit/fs1/board.cmake
@@ -1,6 +1,4 @@
 
 board_runner_args(jlink "--device=TMS570LS1224" "--iface=jtag" "--speed=4000")
-board_runner_args(openocd --target-handle=_CHIPNAME.cpu0)
 
-include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
