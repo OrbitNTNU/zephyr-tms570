1fbab9a8e8f786577736be7ec96f4e6803f3fac2
sigmundk <sigmund.klapbakken@orbitntnu.com>
2026-02-18 21:14:26 +0100

    drivers: gpio: Support non-GIO ports

--- drivers/gpio/gpio_tms570.c
+++ drivers/gpio/gpio_tms570.c
@@ -19,8 +19,9 @@
 #define INTOFF2_OFFSET   (0x28)
 
 #define PORT_A_OFFSET      (0x34)
-#define PORT_SIZE          (0x20)
-#define PORT_PINS          (8)
+#define PORT_AB_SIZE       (0x20)
+#define PORT_AB_PINS       (8)
+#define PORT_AB_COUNT      (2)
 #define PORT_DIR_OFFSET    (0x00)
 #define PORT_DIN_OFFSET    (0x04)
 #define PORT_DOUT_OFFSET   (0x08)
@@ -34,6 +35,7 @@ struct gpio_tms570_port_cfg {
         struct gpio_driver_config gpio_cfg;
         uintptr_t reg_base;
         const struct device *parent;
+        bool interrupt_capable;
 };
 
 struct gpio_tms570_port_data {
@@ -45,8 +47,8 @@ struct gpio_tms570_ctrl_cfg {
         uintptr_t reg_base;
         void (*irq_connect)(void);
 
-        const struct device **children;
-        size_t num_children;
+        const struct device **intc_ports;
+        size_t num_intc_ports;
 };
 
 static int gpio_tms570_pin_configure(const struct device *dev, gpio_pin_t pin, gpio_flags_t flags)
@@ -145,12 +147,18 @@ static unsigned int port_to_bit(const struct device *dev, gpio_pin_t pin)
 
         uintptr_t first = ctrl_cfg->reg_base + PORT_A_OFFSET;
 
-        return ((cfg->reg_base - first) / PORT_SIZE) * PORT_PINS + pin;
+        return ((cfg->reg_base - first) / PORT_AB_SIZE) * PORT_AB_PINS + pin;
 }
 
 static int gpio_tms570_manage_callback(const struct device *dev, struct gpio_callback *cb, bool set)
 {
+        const struct gpio_tms570_port_cfg *cfg = dev->config;
         struct gpio_tms570_port_data *data = dev->data;
+
+        if (!cfg->interrupt_capable) {
+                return -ENOTSUP;
+        }
+
         return gpio_manage_callback(&data->cb, cb, set);
 }
 
@@ -162,6 +170,10 @@ static int gpio_tms570_pin_interrupt_configure(const struct device *dev, gpio_pi
         uintptr_t reg_base = ctrl_cfg->reg_base;
         unsigned int bit;
         int status;
+
+        if (!cfg->interrupt_capable) {
+                return -ENOTSUP;
+        }
 
         bit = port_to_bit(dev, pin);
         status = 0;
@@ -192,7 +204,7 @@ static int gpio_tms570_pin_interrupt_configure(const struct device *dev, gpio_pi
         return status;
 }
 
-static DEVICE_API(gpio, gpio_tms570_api) = {
+static DEVICE_API(gpio, gpio_tms570_port_api) = {
         .pin_configure = gpio_tms570_pin_configure,
         .port_get_raw = gpio_tms570_port_get_raw,
         .port_set_masked_raw = gpio_tms570_port_set_masked_raw,
@@ -212,22 +224,22 @@ static int gpio_tms570_port_init(const struct device *dev)
 static void gpio_tms570_ctrl_isr(const struct device *dev)
 {
         const struct gpio_tms570_ctrl_cfg *ctrl_cfg = dev->config;
-        const struct device *child;
+        const struct device *port;
         struct gpio_tms570_port_data *port_data;
         uint32_t reg;
         gpio_pin_t pin;
 
         /* We currently only use low-priority interrupts. */
         reg = sys_read32(ctrl_cfg->reg_base + INTOFF2_OFFSET);
-        if (reg == 0 || reg > ctrl_cfg->num_children * PORT_PINS) {
+        if (reg == 0 || reg > PORT_AB_COUNT * PORT_AB_PINS) {
                 return;
         }
 
-        pin = (reg - 1) % PORT_PINS;
-        child = ctrl_cfg->children[(reg - 1) / PORT_PINS];
-        port_data = child->data;
+        pin = (reg - 1) % PORT_AB_PINS;
+        port = ctrl_cfg->intc_ports[(reg - 1) / PORT_AB_PINS];
+        port_data = port->data;
 
-        gpio_fire_callbacks(&port_data->cb, child, BIT(pin));
+        gpio_fire_callbacks(&port_data->cb, port, BIT(pin));
 }
 
 static int gpio_tms570_ctrl_init(const struct device *dev)
@@ -245,11 +257,12 @@ static int gpio_tms570_ctrl_init(const struct device *dev)
                 .gpio_cfg.port_pin_mask = GPIO_PORT_PIN_MASK_FROM_DT_NODE(node),                   \
                 .reg_base = DT_REG_ADDR(node),                                                     \
                 .parent = DEVICE_DT_GET(DT_PARENT(node)),                                          \
+                .interrupt_capable = DT_PROP(node, interrupt_capable),                             \
         };                                                                                         \
         static struct gpio_tms570_port_data gpio_tms570_port_##node##_data;                        \
         DEVICE_DT_DEFINE(node, gpio_tms570_port_init, NULL, &gpio_tms570_port_##node##_data,       \
                          &gpio_tms570_port_##node##_cfg, PRE_KERNEL_1, CONFIG_GPIO_INIT_PRIORITY,  \
-                         NULL)
+                         &gpio_tms570_port_api)
 
 #define GPIO_TMS570_CONTROLLER_IRQ_DEFINE(inst)                                                    \
         static void gpio_tms570_ctrl_##inst##_irq_connect(void)                                    \
@@ -261,14 +274,27 @@ static int gpio_tms570_ctrl_init(const struct device *dev)
 
 #define GPIO_TMS570_CONTROLLER_IRQ_INIT(inst) .irq_connect = gpio_tms570_ctrl_##inst##_irq_connect,
 
+/* Get the pointer to an interrupt-capable port. If the given node does not refer to an
+ * interrupt-capable port,
+ * () is returned. If the node does not have status okay, NULL is returned to preserve the order
+ * specified in devicetree.
+ *
+ * This assumes that the GPIO ports in DTS are defined with ascending memory address (A, B, C and so
+ * on). */
+#define GPIO_TMS570_CONTROLLER_INTC_PORT_ITEM(node)                                                \
+        COND_CODE_1(                                                                               \
+                DT_PROP(node, interrupt_capable),                                                  \
+                (COND_CODE_1(DT_NODE_HAS_STATUS(node, okay), (DEVICE_DT_GET(node), ), (NULL, ))),  \
+                ())
+
 #define GPIO_TMS570_CONTROLLER_INIT(inst)                                                          \
         GPIO_TMS570_CONTROLLER_IRQ_DEFINE(inst);                                                   \
-        static const struct device *gpio_tms570_ctrl_##inst##_ports[] = {                          \
-                DT_INST_FOREACH_CHILD_STATUS_OKAY_SEP(inst, DEVICE_DT_GET, (, ))};                 \
+        static const struct device *gpio_tms570_ctrl_##inst##_intc_ports[] = {                     \
+                DT_INST_FOREACH_CHILD(inst, GPIO_TMS570_CONTROLLER_INTC_PORT_ITEM)};               \
         static const struct gpio_tms570_ctrl_cfg gpio_tms570_ctrl_##inst##_cfg = {                 \
                 .reg_base = DT_INST_REG_ADDR(inst),                                                \
-                .children = gpio_tms570_ctrl_##inst##_ports,                                       \
-                .num_children = ARRAY_SIZE(gpio_tms570_ctrl_##inst##_ports),                       \
+                .intc_ports = gpio_tms570_ctrl_##inst##_intc_ports,                                \
+                .num_intc_ports = ARRAY_SIZE(gpio_tms570_ctrl_##inst##_intc_ports),                \
                 GPIO_TMS570_CONTROLLER_IRQ_INIT(inst)};                                            \
         DEVICE_DT_INST_DEFINE(inst, gpio_tms570_ctrl_init, NULL, NULL,                             \
                               &gpio_tms570_ctrl_##inst##_cfg, PRE_KERNEL_1,                        \
